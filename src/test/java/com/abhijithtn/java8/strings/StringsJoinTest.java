d4ca6ded4926d4bcaadf1943799e6f56781c9024
Abhijith Nagarajan <abhijithtn@gmail.com>
2014-10-05 00:27:18 +0200

    Strings join Java8

--- src/test/java/com/abhijithtn/java8/strings/StringsJoinTest.java
+++ src/test/java/com/abhijithtn/java8/strings/StringsJoinTest.java
@@ -1,19 +1,34 @@
 package com.abhijithtn.java8.strings;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class StringsJoinTest {
 
-    @Before
-    public void setUp() throws Exception {
+    @Test
+    public void testJoinStrings() throws Exception {
 
+        String[] strArr = new String[]{"Jai","Sri","Rama"};
+
+        String output = StringsJoin.joinStrings(" ",strArr);
+
+        Assert.assertEquals("Jai Sri Rama", output);
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void testJoinIterableStrings() throws Exception {
+
+        Iterable<String> strIterable = Arrays.asList("Jai","Sri","Rama");
+
+        String output = StringsJoin.joinIterableStrings(" ",strIterable);
+
+        Assert.assertEquals("Jai Sri Rama", output);
 
     }
 }
